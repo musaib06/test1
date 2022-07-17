b6de5551fb2e839392ce7889bdd2c8064fae1651
musaib06 <musaib709@.com>
2022-07-17 14:31:28 +0530

    Adding some changes to the project by musaib

--- src/app/exam-dashboard/exam-dashboard.component.ts
+++ src/app/exam-dashboard/exam-dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxIndexedDBService } from 'ngx-indexed-db';
+import { element, Key } from 'protractor';
+import { idText } from 'typescript';
 
 import { QuestionAnswers } from './exam.model';
 
@@ -13,50 +15,52 @@ export class ExamDashboardComponent implements OnInit {
   submitted: boolean = false;
   displayStyle = 'none';
   public index: number = 0;
+
   public ans: string = '';
   constructor(private dbService: NgxIndexedDBService) {}
   ngOnInit(): void {
     this.getAllData();
   }
   getAllData() {
-    this.dbService.getAll('adminTable').subscribe((res) => {
+  
+    this.dbService.getByID('adminTable',4).subscribe((res) => {
       // res.forEach(async (questionDB) => {
-      //   let question = await this.addQuestionToUI(questionDB);
-      //   this.quiz.push(question);
+      //   // let question = await this.addQuestionToUI(questionDB);
+      //   this.quiz
       // });
-      // this.quiz = res;
-      // console.log(this.quiz);
+      this.quiz= res;
+      console.log(this.quiz);
     });
   }
-  async addQuestionToUI(questionDb: any) {
-    let question: QuestionAnswers = {
-      id: questionDb.id,
-      question: questionDb.question,
-      options: [],
-    };
+  // async addQuestionToUI(questionDb: any) {
+  //   // let question: QuestionAnswers = {
+  //   //   id: questionDb.id,
+  //   //   question: questionDb.question,
+  //   //   options: [],
+  //   // };
 
-    let answers = await this.getAnswersByQuestionId(questionDb.id);
-    // question.options.push(...answers);
-    // answers.forEach((answer) => {
-    //   question.options.push({
-    //     answerId: answer.id,
-    //     answerDescription: answer.answer,
-    //     isCorrect: answer.isCorrect,
-    //     questionId: answer.question_id,
-    //   });
-    // });
-    return question;
-  }
+  //   // let answers = await this.getAnswersByQuestionId(questionDb.id);
+  //   // question.options.push(...answers);
+  //   // answers.forEach((answer) => {
+  //   //   question.options.push({
+  //   //     answerId: answer.id,
+  //   //     answerDescription: answer.answer,
+  //   //     isCorrect: answer.isCorrect,
+  //   //     questionId: answer.question_id,
+  //   //   });
+  //   // });
+  //   // return question;
+  // }
 
-  async getAnswersByQuestionId(id: number) {
-    await this.dbService
-      .getByIndex('userResponse', 'question_Id', id)
-      .subscribe(async (res) => {
-        // this.quiz = res;
-        return res;
-        // console.log(quiz);
-      });
-  }
+  // async getAnswersByQuestionId(id: number) {
+  //   // await this.dbService
+  //   //   .getByIndex('userResponse', 'question_Id', id)
+  //   //   .subscribe(async (res) => {
+  //   //     // this.quiz = res;
+  //   //     return res;
+  //   //     // console.log(quiz);
+  //   //   });
+  // }
   submitResponse(selectedItem) {
     let answer = this.ans;
     let question_id = selectedItem.id;
